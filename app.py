217787c2895a47988c85077ddac91513c156ee5f
Dhava2010 <87840766+Dhava2010@users.noreply.github.com>
2024-01-01 18:40:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+global app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    Label('Hello There!').pack()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 8080)
+    app.run()
