9c94756a0cb7ec137e4bca5d923e24f2e3decbd4
Dhava2010 <87840766+Dhava2010@users.noreply.github.com>
2024-01-01 19:00:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from tkinter import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    Label('Hello There!').pack()
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
